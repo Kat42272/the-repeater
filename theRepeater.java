50a3ac41ded2019b56c3338f674ae57940d721d5
Kathryn Anderson <kat42272@yahoo.com>
2020-07-15 15:35:09 -0700

    First basic java file

--- theRepeater.java
+++ theRepeater.java
@@ -1,1 +1,8 @@
 // the first java game
+
+// create a class named S1P1
+public class theRepeater {
+  public static void main(String[] arg) {
+    System.out.println("Whatever you type here ... will be repeated!");
+  }
+}
